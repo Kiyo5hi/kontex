2cb1127486b1f2e1fe66a5ce81d6759a254bf8c2
Sheey <1511956322@qq.com>
2020-02-18 17:52:27 +0800

    :bug: removed all types declaration.

--- types/routers/index.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference types="koa__router" />
-import Router from "@koa/router";
-import { DefaultState, Context } from "koa";
-declare const router: Router<DefaultState, Context>;
-export { router };
